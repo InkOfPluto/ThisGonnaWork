67079380e3ffcbd79dbb1919e8fffe1dcad4d0fa
RJL Goh <rahaulgoh@gmail.com>
2025-02-10 16:11:55 +0000

    Ghost Script added, offsetCube added

--- Assets/GhostScript.cs
+++ Assets/GhostScript.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class GhostScript : MonoBehaviour
 {
 
-    public Vector3 offset;
-    public Transform target; 
+    public Transform vrHand;
+    public Transform robotHand; 
+    public Vector3 positionOffset;
+    public Vector3 rotationalOffset;
 
     void Start()
     {
@@ -16,6 +18,26 @@ public class GhostScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(vrHand != null && robotHand != null)
+        {
+            //Positional offset
+            Vector3 robotPosOffset = vrHand.transform.position + positionOffset;
+
+            // // Rotational offset
+            Vector3 robotRotOffset = vrHand.transform.rotation.eulerAngles + rotationalOffset;
+
+            //Apply offsets to robot hand
+            robotHand.position = robotPosOffset;
+            robotHand.rotation = Quaternion.Euler(robotRotOffset);
+        }
     }
+
+    // public void SetOffset(Vector3 posOffset, Vector3 rotOffset)
+    // {
+    //     positionOffset = posOffset;
+    //     rotationalOffset = rotOffset;
+    // }
+
+    //Ghost object, shares same properties as the object that the robot will pick up
+    //Grasp when we grasp.
 }
