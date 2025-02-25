5481a6cc0fcd8eda1c474cbddf4d09b5073a8bba
chinmay khedkar <crkhedkar@gmail.com>
2025-02-25 15:29:00 +0000

    robot wrist prefab, new script

--- Assets/ArticulationPrismDriverGripper.cs
+++ Assets/ArticulationPrismDriverGripper.cs
@@ -18,6 +18,17 @@ public class ArticulationPrismDriverGripper : MonoBehaviour
     public Vector3 RotationOffset = new Vector3();
     public Vector3 PositionOffset = new Vector3();
 
+    // Tip gameObjects for calculating the relative distance from tip to palm, let me know what you think.
+    public GameObject thumbTip, indexTip, midTip, ringTip, palm;
+
+
+    // Close states for the mapping function, not sure about the value might have to think about it again.
+    public ArticulationBody tmb, index, mid, ring;
+    float tmbCloseState = 0.05f;
+    float indexCloseState = 0.05f;
+    float midCloseState = 0.05f;
+    float ringCloseState = 0.05f;
+
     bool start; 
 
     private void Start()
@@ -26,6 +37,7 @@ public class ArticulationPrismDriverGripper : MonoBehaviour
         Invoke("FindGripper", 1f); 
     }
 
+    // Do we assign these to the robot wrist digits on the prefab? They currently are unassigned in Unity
     void FindGripper()
     {
         physicsObjects[0] = GameObject.FindWithTag("wrist").GetComponent<ArticulationBody>();
@@ -96,7 +108,54 @@ public class ArticulationPrismDriverGripper : MonoBehaviour
 
     void Grip()
     {
+        float vrThumbValue = GetVRThumbValue();
+        float vrIndexValue = GetVRIndexValue();
+        float vrMiddleValue = GetVRMiddleValue();
+        float vrRingValue = GetVRRingValue();
 
+        // 1. Finger A and B to close on the object (to finger close state) 
+        ArticulationDrive thumb_digit = physicsObjects[1].yDrive;
+        thumb_digit.target = mapThumb(vrThumbValue, 0f, 1f, tmbCloseState, 0f); // Replaced the zero value with this, does it make sense?
+        physicsObjects[1].yDrive = thumb_digit;
+
+        ArticulationDrive index_digit = physicsObjects[2].yDrive;
+        index_digit.target = mapIndex(vrIndexValue, 0f, 1f, indexCloseState, 0f);
+        physicsObjects[2].yDrive = index_digit;
+
+        ArticulationDrive mid_digit = physicsObjects[3].zDrive;
+        mid_digit.target = mapRing(vrMiddleValue, 0f, 1f, midCloseState, 0f);
+        physicsObjects[3].zDrive = mid_digit;
+
+        ArticulationDrive ring_digit = physicsObjects[4].zDrive;
+        ring_digit.target = mapRing(vrRingValue, 0f, 1f, ringCloseState, 0f);
+        physicsObjects[4].zDrive = ring_digit;
+    }
+
+    // Placeholder to get VR finger values: I added in the tip gameObjects for reach finger in order to get their relative value to the palm, not sure how to implement this.
+    private float GetVRThumbValue() { return 0.5f; }
+    private float GetVRIndexValue() { return 0.5f; }
+    private float GetVRMiddleValue() { return 0.5f; }
+    private float GetVRRingValue() { return 0.5f; }
+
+
+    // Mapping function to get the value between 0 and 1.
+    public static float mapIndex(float value, float leftMin, float leftMax, float rightMin, float rightMax)
+    {
+        return rightMin + (value - leftMin) * (rightMax - rightMin) / (leftMax - leftMin);
+    }
+    public static float mapMiddle(float value, float leftMin, float leftMax, float rightMin, float rightMax)
+    {
+        return rightMin + (value - leftMin) * (rightMax - rightMin) / (leftMax - leftMin);
+    }
+    public static float mapRing(float value, float leftMin, float leftMax, float rightMin, float rightMax)
+    {
+        return rightMin + (value - leftMin) * (rightMax - rightMin) / (leftMax - leftMin);
+    }
+    public static float mapThumb(float value, float leftMin, float leftMax, float rightMin, float rightMax)
+    {
+        return rightMin + (value - leftMin) * (rightMax - rightMin) / (leftMax - leftMin);
     }
 
 }
+
+// Are we going to basically close the robot digit when say the relative thumbTip position to the palm is > 0.7? This makes sense in my head but I don't know if it fully works, let me know.
